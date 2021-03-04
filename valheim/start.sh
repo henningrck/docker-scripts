06c2d353fcc0ce5a61e5590018f80a66428d23bc
Henning Storck <mail@henningstorck.com>
2021-03-04 20:06:09 +0100

    Fix broken LD_LIBRARY_PATH

--- valheim/start.sh
+++ valheim/start.sh
@@ -5,6 +5,6 @@ export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
-/home/steam/valheim/server/valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world $WORLD_NAME -password $PASSWORD -savedir "/home/steam/valheim/data" -public 1
+./valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world $WORLD_NAME -password $PASSWORD -savedir "/home/steam/valheim/data" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
